1aab30fb315b5400381157a3ab10bb1a8c31e71d
KenseyDaly <kensey.daly@jacobs.com>
2021-06-17 09:41:28 -0700

    Moved text to above steps per Daya's comment.

--- CaUWMET/src/apps/home.py
+++ CaUWMET/src/apps/home.py
@@ -34,14 +34,16 @@ def app():
         and costs of changes to urban water supply reliability. This tool utilizes the principle 
         of least-cost-planning to provide users information on 1) the cost of a water supply reliability
         enhancement and 2) the cost of unreliability.""")
+        st.write("")
+        st.write("""Users can learn about the model approach, edit model assumptions and review results by navigating through the pages in the navigation tree
+        to the left. If you're curious or would like to modify the source code, please visit the GitHub repository at: <repository link>.
+        Additional information on the model approach, documentation, and video tutorials can be found in the Model Overview page.""")        
 
         local_css("style.css")
         t = """<div><span class='font'>
         Steps to use this tool are summarized in green below. </span></div>"""
         st.markdown(t, unsafe_allow_html=True)
-        st.write("""Users can learn about the model approach, edit model assumptions and review results by navigating through the pages in the navigation tree
-        to the left. If you're curious or would like to modify the source code, please visit the GitHub repository at: <repository link>.
-        Additional information on the model approach, documentation, and video tutorials can be found in the Model Overview page.""") 
+ 
         
         local_css("style.css")
         t = """<div><span class='font'>
