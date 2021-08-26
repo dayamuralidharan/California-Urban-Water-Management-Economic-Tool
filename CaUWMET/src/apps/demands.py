a2479f1084e15d776d8b4f5000210769bcde089c
Rajagopalan <Niranjana.Rajagopalan@jacobs.com>
2021-08-26 09:25:59 -0700

    Deleted Demands Management Page. Added Demands Management graphs to Demands page. Added Tooltip feature

--- CaUWMET/src/apps/demands.py
+++ CaUWMET/src/apps/demands.py
@@ -7,20 +7,20 @@ from itertools import cycle
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
 import traceback
 from load_css import local_css
-from demandsHelper import load_data, summary_poster
 from contextlib import contextmanager
 import sys, os
 from appsUtilities import opt_echo
+from demandsHelper import load_data, summary_poster
 
 def setTotalDemandsInputData():
     if st.session_state.totalDemandsChoice == 'UWMP reported values':
-        st.session_state.totalDemandsdf = load_data("inputData/totalDemandsData.csv")
+        st.session_state.totalDemandsdf = load_data("inputData/totalDemands.csv")
         st.session_state.demandsPlotInputdf = load_data("inputData/totalDemandsGraphData.csv")
     elif st.session_state.totalDemandsChoice == 'ETAW adjusted demands':
-        st.session_state.totalDemandsdf = load_data("inputData/totalDemandsData.csv") ################################ Data needs updating
+        st.session_state.totalDemandsdf = load_data("inputData/totalDemands.csv") ################################ Data needs updating
         st.session_state.demandsPlotInputdf = load_data("inputData/totalDemandsGraphData.csv") ################################ Data needs updating
     else:
-        st.session_state.totalDemandsdf = load_data("inputData/totalDemandsData.csv") ################################ Data needs updating
+        st.session_state.totalDemandsdf = load_data("inputData/totalDemands.csv") ################################ Data needs updating
         st.session_state.demandsPlotInputdf = load_data("inputData/totalDemandsGraphData.csv") ################################ Data needs updating
 
 
@@ -29,13 +29,13 @@ def app():
 # "with" makes sure any memory resources used by this page gets closed so its not taking memory when the page is closed. 
     with opt_echo():
         # Initialize Session State with Default Values
-        st.session_state['totalDemandsdf'] = load_data("inputData/totalDemandsData.csv")
+        st.session_state['totalDemandsdf'] = load_data("inputData/totalDemands.csv")
         st.session_state['demandsPlotInputdf'] = load_data("inputData/totalDemandsGraphData.csv")
 
         #Set font styling (currently used for green text)
         local_css("style.css")
 
-        st.title('Demand Assumptions Page')
+        st.title('Demand and Demand Management Assumptions Page')
         st.header("Steps to use this page")
 
 
@@ -49,14 +49,14 @@ def app():
         
         demandsDatasetOptions = ['UWMP reported values', 'ETAW adjusted demands', 'Input demands in table below']
         st.radio("""1. Select the Total Demand Scenario Dataset from the options below. If the last option is selected, 
-        update the data in the Total Demand Scenarios table in the first collapsible section below.""", demandsDatasetOptions, key = "totalDemandsChoice", on_change = setTotalDemandsInputData)
+        update the data in the Total Demand Scenarios table in the first collapsible section below.""", demandsDatasetOptions, key = "totalDemandsChoice", on_change = setTotalDemandsInputData, help="explanation of each option to be added")
 
         useBySectorDatasetOptions = ['UWMP reported values', 'Input Use By Sector in table below']
-        useBySectorDatasetChoice = st.radio("2. Select the Use by Sector Dataset from the options below. If the last option is selected, update the data in the Demand Use by Sector table in the second collapsible section below.", useBySectorDatasetOptions)
+        useBySectorDatasetChoice = st.radio("2. Select the Use by Sector Dataset from the options below. If the last option is selected, update the data in the Demand Use by Sector table in the second collapsible section below.", useBySectorDatasetOptions, help="explanation of each option to be added")
 
         intExtUseBySectorDatasetOptions = ['UWMP reported values', 'Input Use By Sector in table below']
         intExtUseBySectorDatasetChoice = st.radio("""3. Select the Input Interior and Exterior Use by Sector Dataset from the options below. 
-        If the last option is selected, update the data in the Interior and Exterior Use by Sector table in the third collapsible section below.""", intExtUseBySectorDatasetOptions)
+        If the last option is selected, update the data in the Interior and Exterior Use by Sector table in the third collapsible section below.""", intExtUseBySectorDatasetOptions, help="explanation of each option to be added")
 
         print(st.session_state.totalDemandsdf)
         
@@ -87,11 +87,11 @@ def app():
         sorted_contractors = demandsPlotInputData.groupby('Year')['Contractor'].sum()\
             .sort_values(ascending=True).index
 
-        col1, col2 = st.beta_columns(2)
+        col1, col2 = st.columns(2)
         with col1:
             st.markdown("#### **Select Future Planning Year:**")
             select_contractor = []
-            select_contractor.append(st.selectbox('', sorted_contractors, key='1'))  
+            select_contractor.append(st.selectbox('', sorted_contractors, key='1', help="explanation for tooltip to be added"))  
         
         with col2:
             st.markdown("#### **Select Year Type:**")
@@ -103,10 +103,10 @@ def app():
             sorted_demands = contractor_df.groupby('Type')['Contractor'].count()\
                 .sort_values(ascending=True).index
 
-            select_demands.append(st.selectbox('', sorted_demands, key='2'))
+            select_demands.append(st.selectbox('', sorted_demands, key='2', help="explanation for tooltip to be added"))
             demands_df = contractor_df[contractor_df['Type'].isin(select_demands)]
         
-        col1, col2 = st.beta_columns(2)
+        col1, col2 = st.columns(2)
 
         #Setting up color palette dict
         color_dict = dict(zip(color_map_df['Study Region'], color_map_df['colors']))
@@ -132,11 +132,11 @@ def app():
         street cleaning, line flushing, construction meters, conveyance losses, etc. Agriculture use represents the agricultural related demands within a contractor's service area, such as irrigation. 
         Landscape irrigation includes nonresidential irrigation for areas such as schools and parks, but excludes golf courses which are incorporated in the commercial use sector. More information can be found in the model documentation <add hyperlink to interior and exterior use section of model documentation.>""")
         
-        col1, col2 = st.beta_columns(2)
+        col1, col2 = st.columns(2)
         with col1:
             st.markdown("#### **Select Future Planning Year:**")
             select_contractor = []
-            select_contractor.append(st.selectbox('', sorted_contractors, key='3'))  
+            select_contractor.append(st.selectbox('', sorted_contractors, key='3', help="explanation for tooltip to be added"))  
         
         with col2:
             st.markdown("#### **Select Sector:**")
@@ -148,11 +148,11 @@ def app():
             sorted_demands = contractor_df.groupby('Type')['Contractor'].count()\
                 .sort_values().index
 
-            select_demands.append(st.selectbox('', sorted_demands, key='4'))
+            select_demands.append(st.selectbox('', sorted_demands, key='4', help="explanation for tooltip to be added"))
             demands_df = contractor_df[contractor_df['Type'].isin(select_demands)]
 
         
-        col1, col2 = st.beta_columns(2)
+        col1, col2 = st.columns(2)
         st.text("")
         fig = summary_poster(demands_df, color_dict)
         st.write(fig)
@@ -173,11 +173,11 @@ def app():
         for reuse and wastewater treatment costs. Interior use that results in water available for reuse includes urban wastewater that is deep percolated from septic tanks, while 
         exterior use includes irrigation infiltrated to groundwater systems. More information can be found in the model documentation <add hyperlink to interior and exterior use section of model documentation.>""")
 
-        col1, col2 = st.beta_columns(2)
+        col1, col2 = st.columns(2)
         with col1:
             st.markdown("#### **Select Future Planning Year:**")
             select_contractor = []
-            select_contractor.append(st.selectbox('', sorted_contractors, key='5'))  
+            select_contractor.append(st.selectbox('', sorted_contractors, key='5', help="explanation for tooltip to be added"))  
         
         with col2:
             st.markdown("#### **Select Sector:**")
@@ -189,19 +189,59 @@ def app():
             sorted_demands = contractor_df.groupby('Type')['Contractor'].count()\
                 .sort_values(ascending=True).index
 
-            select_demands.append(st.selectbox('', sorted_demands, key='6'))
+            select_demands.append(st.selectbox('', sorted_demands, key='6', help="explanation for tooltip to be added"))
             demands_df = contractor_df[contractor_df['Type'].isin(select_demands)]
 
-        col1, col2 = st.beta_columns(2)
+        col1, col2 = st.columns(2)
         fig = summary_poster(demands_df, color_dict)
         st.write(fig)
+
+        #---------------------------------------------------------------#
+        # CREATE SUMMARY POSTER FOR BASE LONG-TERM CONSERVATION
+        #---------------------------------------------------------------#
+
+        baseLongTermConservationPlotInputData = load_data("inputData/baseLongTermConservationGraphData.csv")
+    
+        sorted_contractors = baseLongTermConservationPlotInputData.groupby('Year')['Contractor'].sum()\
+            .sort_values().index
+
+        st.subheader("Base Long-Term Conservation")
+        st.write("""Long-term conservation is incorporated into the model as it is an important component of each contractor's long-term strategy to increase 
+        their water supply reliability. These demand reductions typically include programs that target water use efficiency in each sector, turf replacement programs, 
+        conservation outreach and communications, reducing conveyance losses, and more. Long-term demand management measures that are adopted by water users can 
+        have a demand hardening effect. Although they can increase reliability by reducing the size, frequency and duration of shortage events, they can make these 
+        events relatively more costly when they do occur.  A hardening factor can be set to simulate this effect.>""")
+
+        col1, col2 = st.columns(2)
+        with col1:
+            st.markdown("#### **Select Future Planning Year:**")
+            select_contractor = []
+            select_contractor.append(st.selectbox('', sorted_contractors, key='7', help="explanation for tooltip to be added"))  
+        
+        with col2:
+            st.markdown("#### **Select Sector:**")
+            select_demands = []
+
+            #Filter df based on selection
+            contractor_df = baseLongTermConservationPlotInputData[baseLongTermConservationPlotInputData['Year'].isin(select_contractor)]
+
+            sorted_demands = contractor_df.groupby('Type')['Contractor'].count()\
+                .sort_values(ascending=True).index
+
+            select_demands.append(st.selectbox('', sorted_demands, key='8', help="explanation for tooltip to be added"))
+            demands_df = contractor_df[contractor_df['Type'].isin(select_demands)]
+
+        col1, col2 = st.columns(2)
+        fig = summary_poster(demands_df, color_dict)
+        st.write(fig)
+
 
         #---------------------------------------------------------------#
         # COLLAPSIBLE SECTIONS WTIH EDITABLE TABLES
         #---------------------------------------------------------------#
 
         ########################### TABLE 1 - TOTAL DEMAND SCENARIOS
-        with st.beta_expander("Total Demand Scenarios (default values as reported by 2020 UWMPs)"):
+        with st.expander("Total Demand Scenarios (default values as reported by 2020 UWMPs)"):
 
             #Infer basic colDefs from dataframe types
             gb = GridOptionsBuilder.from_dataframe(st.session_state.totalDemandsdf)
@@ -291,7 +331,7 @@ def app():
 
 
         ##########################  TABLE 2 USE BY SECTOR
-        with st.beta_expander("Demand Scenarios By Sector"):
+        with st.expander("Demand Scenarios By Sector"):
             
             @st.cache(suppress_st_warning=True)
             def fetch_data(samples):
@@ -367,7 +407,7 @@ def app():
 
 
         ########################### TABLE 3 INTERIOR AND EXTERIOR USE BY SECTOR
-        with st.beta_expander("Interior and Exterior Use by Sector"):
+        with st.expander("Interior and Exterior Use by Sector"):
 
             @st.cache(suppress_st_warning=True)
             def fetch_data(samples):
@@ -416,6 +456,80 @@ def app():
             with st.spinner("Displaying results..."):
                 #displays the chart
                 chart_data = intExtUsebySectorEditableTabledf.loc[:,['Contractor','2025','2030','2035', '2040', '2045']].assign(source='total')
+
+                if not selected_df.empty:
+                    selected_data = selected_df.loc[:,['Contractor','2025','2030','2035','2040', '2045']].assign(source='selection')
+                    chart_data = pd.concat([chart_data, selected_data])
+
+                chart_data = pd.melt(chart_data, id_vars=['Contractor','source'], var_name="Year", value_name="Water Demand (acre-feet/year)")
+                
+                #st.dataframe(chart_data)
+                chart = alt.Chart(data=chart_data).mark_bar().encode(
+                    x=alt.X("Year:O", axis=alt.Axis(labelAngle=0)),
+                    y=alt.Y("sum(Water Demand (acre-feet/year)):Q", stack=False),
+                    color=alt.Color('source:N', scale=alt.Scale(domain=['total','selection'])),
+                ).configure_axis(
+                    labelFontSize=13,
+                    titleFontSize=13
+                )
+
+                st.markdown("""
+                Users can select multiple agencies from the table above to highlight in the chart below:
+                """)
+
+                st.altair_chart(chart, use_container_width=True)
+                st.write(st.session_state)
+
+        ########################### TABLE 4 BASE LONG-TERM CONSERVATION
+        with st.expander("Base Long-Term Conservation"):
+
+            @st.cache(suppress_st_warning=True)
+            def fetch_data(samples):
+                demands = pd.read_csv("inputData/baseLongTermConservation.csv")
+                return pd.DataFrame(demands)    
+
+            baseLongTermConservationEditableTabledf = fetch_data(sample_size)
+
+            #Infer basic colDefs from dataframe types
+            gb = GridOptionsBuilder.from_dataframe(baseLongTermConservationEditableTabledf)
+
+            #customize gridOptions
+            gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc='sum', editable=True)
+
+            gb.configure_column("2025", type=["numericColumn", "numberColumnFilter", "customNumericFormat"], precision=1, aggFunc='sum')
+            gb.configure_column("2030", type=["numericColumn", "numberColumnFilter", "customNumericFormat"], precision=1, aggFunc='sum')
+            gb.configure_column("2035", type=["numericColumn", "numberColumnFilter", "customNumericFormat"], precision=1, aggFunc='sum')
+            gb.configure_column("2040", type=["numericColumn", "numberColumnFilter", "customNumericFormat"], precision=1, aggFunc='sum')
+            gb.configure_column("2045", type=["numericColumn", "numberColumnFilter", "customNumericFormat"], precision=1, aggFunc='sum')
+            gb.configure_column("Notes", type=["textColumn"])
+
+            gb.configure_grid_options(domLayout='normal')
+            gridOptions = gb.build()
+
+            #Display the grid
+            grid_response = AgGrid(
+                baseLongTermConservationEditableTabledf, 
+                gridOptions=gridOptions,
+                # height=grid_height, 
+                width='100%',
+                data_return_mode=return_mode_value, 
+                update_mode=update_mode_value,
+                # fit_columns_on_grid_load=fit_columns_on_grid_load,
+                allow_unsafe_jscode=True, #Set it to True to allow jsfunction to be injected
+                # enable_enterprise_modules=enable_enterprise_modules,
+                )
+
+            baseLongTermConservationEditableTabledf = grid_response['data']
+            selected = grid_response['selected_rows']
+            selected_df = pd.DataFrame(selected)
+
+            if st.button('Download Dataframe to CSV format', key = "Base Long-Term Conservation"):
+                tmp_download_link = download_link(baseLongTermConservationEditableTabledf, 'Base_Long_Term_Conservation.csv', 'Click here to download your data!')
+                st.markdown(tmp_download_link, unsafe_allow_html=True)
+
+            with st.spinner("Displaying results..."):
+                #displays the chart
+                chart_data = baseLongTermConservationEditableTabledf.loc[:,['Contractor','2025','2030','2035', '2040', '2045']].assign(source='total')
 
                 if not selected_df.empty:
                     selected_data = selected_df.loc[:,['Contractor','2025','2030','2035','2040', '2045']].assign(source='selection')
