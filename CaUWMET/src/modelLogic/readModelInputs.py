277bbde7c0342b375284d1588254bd0c6b78d7e6
Daly <Kensey.Daly@jacobs.com>
2021-10-13 14:07:06 -0700

    Cleaned up setting up of demands dataframe.

--- CaUWMET/src/modelLogic/readModelInputs.py
+++ CaUWMET/src/modelLogic/readModelInputs.py
@@ -90,7 +90,7 @@ demandsAfterBaseConservation = {'Year': historicHydrologyYears}
 contractorsList = list(hydroRegionDf['Contractor'].values) ####TODO: contractors will need to be dynamically read in from streamlit instead of read in from hydro region dataframe
 
 # Set up contractor total demand time series based on hydrologic year type.
-contractor = contractorsList[-1]
+#contractor = contractorsList[-1]
 for contractor in contractorsList:
     contractorRegion = lookupCorrespondingValue(hydroRegionDf, contractor, colA='Contractor', colB='Hydro. Region')
     if contractorRegion == 'San Joaquin':
@@ -117,6 +117,7 @@ for contractor in contractorsList:
     #demandsAfterBaseConservation[contractor] = demandsAfterBaseConservation
 
 totalDemands = pd.DataFrame(totalDemands)
+
 #demandsAfterBaseConservation = pd.DataFrame(demandsAfterBaseConservation)
 
 
