4f3abde4446c30a9a940880e40091fba07899e91
sainjacobs <sai.nudurupati@jacobs.com>
2021-10-26 17:15:56 -0700

    Updated files while performing QA
    Have minor corrections to the code. QA process has been performed.

--- CaUWMET/src/extractCalsimIITimeseriesData.py
+++ CaUWMET/src/extractCalsimIITimeseriesData.py
@@ -3,9 +3,15 @@
 @date: 20211011
 @author: Sai Nudurupati (sai.nudurupati@jacobs.com)
 @description: Code to extract CalSim II timeseries output data from the DSS file.
+Reads inputs from:
+- inputData/contractorInformation.csv
+- inputData/calsim_hist_011221_output.dss
+Writes outputs to:
+- inputData/swpCVPSupplyData.csv
 """
 # Import packages
 import os
+import numpy as np
 import pyhecdss
 import pandas as pd
 
@@ -53,9 +59,18 @@ fData = {}
 fPieces = []
 flowNodes = []
 finalExpressions = []
+allNodes = []
+nodePathnames = []
+# Looping through each contractor
 for i, item in enumerate(formula):
-    # Exceptions for the contractors with complicated expressions (three in total). TODO: Fix this.
-    if ("south" in item.lower()) or ("min" in item.lower()) or ("wy" in item.lower()):
+    # Exceptions as of now. TODO: Fix these when possible.
+    if (
+            ("min" in item.lower()) or
+            ("wy" in item.lower())
+    ):
+        finalExpressions.append("Invalid")
+        flowData[i] = np.zeros(83)  # Fill timeseries with zeroes (temporary fix)
+        fData[i] = flowData[i]
         continue
     flowData[i] = None
     fData[i] = []
@@ -65,14 +80,20 @@ for i, item in enumerate(formula):
     fNodes = []
     var = []
     var_counter = 0
+    # Looping through individual nodes, retrieving timeseries and executing expressions
     for j, an_item in enumerate(splitFormula):
         if an_item in partB:
             fNodes.append(an_item)
+            allNodes.append(an_item)
             fdf3=catdf[(catdf.B==an_item)]  # & ('FLOW' in catdf.C)]
+            # Use pyhecdss to retrieve tiimeseries from dss file
             with pyhecdss.DSSFile(inputDSSFile) as d:
                 plist1 = d.get_pathnames(fdf3)
+                nodePathnames.append(plist1[0])
                 dfr1, units1, ptype1 = d.read_rts(plist1[0])
+                # Resample monthly data to yearly
                 dfr2 = dfr1.resample('Y').sum()
+                # Keep data only for required years
                 dfr3 = dfr2[(dfr2.index.year >= 1921) & (dfr2.index.year <= 2003)]
             var.append(dfr3[dfr3.columns[0]].values)
             copyExpression[j] = "var[%d]" % var_counter
@@ -80,6 +101,16 @@ for i, item in enumerate(formula):
     finalExpressions.append("".join(copyExpression))
     flowData[i] = eval("".join(copyExpression))
     fData[i] = flowData[i]
+# Create data frame and align contractor names to yearly timeseries data
 flowDataDf = pd.DataFrame(fData, index=dfr3.index)
 flowDataDf.rename(columns=dict(zip(flowDataDf.columns, nodeMappingDf['Contractor'].values)), inplace=True)
+# Write data to output file
 flowDataDf.to_csv(outputFile)
+
+
+## Optional for QA
+# pathnamesOutput = {}
+# pathnamesOutput['allNodes'] = allNodes
+# pathnamesOutput['nodePathnames'] = nodePathnames
+# pathnamesDataframe = pd.DataFrame(pathnamesOutput)
+# pathnamesDataframe.to_csv(os.path.join(dirname, "outputPathnames.csv"))
