0bbd4f254b974f44fc63f21afcd6131ff1633e94
KenseyDaly <kensey.daly@jacobs.com>
2022-12-30 20:58:18 -0800

    updated elasticity of demands to be unique for each use type

--- CaUWMET/src/modelLogic/contingentWMOsUtilities.py
+++
@@ -1,8 +0,0 @@
-class ContingentWmosUtilities:
-        
-    def implementContingencyConservation(self, demandsToBeMetByWaterMarketTransfers_Contractor, demandsToBeMetByContingentOptions_Contractor, i, contingentConservationUseReductionVolume_Contractor):
-        # Set variables
-        self.demandsToBeMetByWaterMarketTransfers_Contractor = demandsToBeMetByWaterMarketTransfers_Contractor
-        self.demandsToBeMetByContingentOptions_Contractor = demandsToBeMetByContingentOptions_Contractor
-        self.i = i
-        self.contingentConservationUseReductionVolume_Contractor = contingentConservationUseReductionVolume_Contractor
