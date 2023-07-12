ea44be16abe15f61fdb3883f8beae1d9e825e004
Daly <Kensey.Daly@jacobs.com>
2023-07-12 14:16:20 -0700

    Fixed order of long-term wmo upper bounds in optimization method, fixed name of function being executed in the unit tests for model logic.

--- CaUWMET/tests/test_cost_optimization.py
+++ CaUWMET/tests/test_cost_optimization.py
@@ -3,14 +3,3 @@ from src.modelLogic.inputData import InputData
 from src.modelLogic.modelLogic import ModelLogic
 from src.modelLogic.storageUtilities import StorageUtilities
 from tests.inputDataLocationsForTesting import InputDataLocationsForTesting
-from src.modelLogic.costOptimizer import CostOptimizer
-
-# class CostOptimizationTests(TestCase):
-    
-    # def testPolynomialFunctionWasRetrieved(self):
-    #     inputData = InputData(InputDataLocationsForTesting())
-    #     self.modelLogic = ModelLogic(inputData, StorageUtilities())
-    #     self.costOptimizer = CostOptimizer(inputData, self.modelLogic)
-    #     self.costOptimizer.optimizeWMOs()
-        
-        #self.assertIsNotNone(self.costOptimizer.polyCoefficients)
