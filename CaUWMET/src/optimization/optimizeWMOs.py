44cecdfbf6f9839ba3ab38c56f58dafd2da8199b
greenmmq <mark.green1@jacobs.com>
2023-10-12 16:27:29 -0700

    Plot Data to Output
    data from the optimization populations are output to the excel file
    also called as a function and referenced by visualization_a
    updated jupyter notebook and example ModelOutputs_Optimal

--- CaUWMET/src/optimization/optimizeWMOs.py
+++ CaUWMET/src/optimization/optimizeWMOs.py
@@ -97,10 +97,55 @@ class OptimizeWMOs:
 
         print("Best solution found: \nX = %s\nF = %s" % (self.X, self.F))
         print(f"Execution time: {round(self.res.exec_time)} seconds")
-        
+
         if result: 
             return self.res
-    
+
+
+    def preparePlotData(self):
+        # get the particles for plotting
+        Xp = []
+        Fp = []
+        colors = []
+        c = 0
+        try:
+            for h in self.res.history:
+                for p in h.particles:
+                    if self.wmoCeiling is not None:
+                        if np.sum(p._X) < self.wmoCeiling:
+                            Xp.append(p._X)
+                            Fp.append(p._F)
+                            colors.append(c)
+                    else: 
+                        Xp.append(p._X)
+                        Fp.append(p._F)
+                        colors.append(c)
+                c+=1
+            # assign plot variables
+            TAF = np.sum(Xp,axis=1)  # sum of longtermWMOSupply variables
+            ltwmolist = [ list(arr) for arr in zip(*Xp) ]  #results for each ltwmo
+            y_millions = [ f[0]*10**-6 for f in Fp ]  # F in millions
+            plotData = {
+                'contractor': self.modelLogic.contractor,
+                'x': TAF,
+                'y': [ f[0] for f in Fp ],
+                'y_millions': y_millions,
+                'colors': colors,
+                'conservation':	[ round(ltwmo,1) for ltwmo in ltwmolist[0] ],
+                'surface': [ round(ltwmo,1) for ltwmo in ltwmolist[1] ],
+                'groundwater': [ round(ltwmo,1) for ltwmo in ltwmolist[2] ],
+                'desalination': [ round(ltwmo,1) for ltwmo in ltwmolist[3] ],
+                'recycled': [ round(ltwmo,1) for ltwmo in ltwmolist[4] ],
+                'potable_reuse': [ round(ltwmo,1) for ltwmo in ltwmolist[5] ],
+                'transfers_exchanges': [ round(ltwmo,1) for ltwmo in ltwmolist[6] ],
+                'other': [ round(ltwmo,1) for ltwmo in ltwmolist[7] ]
+            }
+            self.plotData = pd.DataFrame(data=plotData)
+        except: 
+            print("Couldn't get plot data! Aborting plot data preparation....")
+            self.plotData = pd.DataFrame(data={})
+            pass
+
     
     def reportBest(self):
         '''
@@ -128,6 +173,7 @@ class OptimizeWMOs:
         Note: 
             Must be run after .optimize method!
         '''
+        self.preparePlotData()
         modelOutputs = {
             'longtermWMOVolumeLimits': pd.DataFrame(data={
                 self.modelLogic.contractor: self.upperBounds if self.X_zero is None else self.X_zero
@@ -166,7 +212,7 @@ class OptimizeWMOs:
             'otherSupplyLongTermWMOCost': self.modelLogic.outputHandler.otherSupplyLongTermWMOCost[self.modelLogic.contractor], # ($)
             'conservationLongTermWMOCost': self.modelLogic.outputHandler.conservationLongTermWMOCost[self.modelLogic.contractor], # ($)
 
-            #System operations costs
+            # System operations costs
             'swpCVPDeliveryCost': self.modelLogic.outputHandler.swpCVPDeliveryCost[self.modelLogic.contractor], # ($)
             'putGroundwaterBankCost': self.modelLogic.outputHandler.putGroundwaterBankCost[self.modelLogic.contractor], # ($)
             'takeGroundwaterBankCost': self.modelLogic.outputHandler.takeGroundwaterBankCost[self.modelLogic.contractor], # ($)
@@ -174,6 +220,9 @@ class OptimizeWMOs:
             'waterTreatmentCost': self.modelLogic.outputHandler.waterTreatmentCost[self.modelLogic.contractor], # ($)
             'distributionCost': self.modelLogic.outputHandler.distributionCost[self.modelLogic.contractor], # ($)
             'wastewaterTreatmentCost': self.modelLogic.outputHandler.wastewaterTreatmentCost[self.modelLogic.contractor], # ($)
+
+            # Optimzation Results for plots
+            'plotData': self.plotData
         }
         
         # QAQC Results
@@ -199,84 +248,67 @@ class OptimizeWMOs:
         This method can be called after the self.res object has been created by the optimize() method. 
         Accessing the optimization history in self.res allows for plotting of the optimization search results.
         Note: 
-            Must be run after .reportZero method!
+            Must be run after .reportZero method and after preparePlotData!
         '''
-        # get the particles 
-        # TODO: this could be its own method...
-        X = []
-        F = []
-        colors = []
-        c = 0
         try:
-            for h in self.res.history:
-                for p in h.particles:
-                    if self.wmoCeiling is not None:
-                        if np.sum(p._X) < self.wmoCeiling:
-                            X.append(p._X)
-                            F.append(p._F)
-                            colors.append(c)
-                    else: 
-                        X.append(p._X)
-                        F.append(p._F)
-                        colors.append(c)
-                c+=1
-        except: 
-            print("Couldn't get particles. Aborting...")
-        
-        # assign plot variables
-        TAF = np.sum(X,axis=1)  # sum of longtermWMOSupply variables
-        loss_millions = [ f*10**-6 for f in F ]
-        
-        # matplotlib
-        fig, ax = plt.subplots(1, 1, figsize=(6, 6))          # setup the plot
-        cmap = plt.cm.viridis                                 # define the colormap
-        cmaplist = [ cmap(i) for i in range(cmap.N) ]           # extract all colors from map
-        cmap = mpl.colors.LinearSegmentedColormap.from_list(  # create the new map
-            'Custom cmap', cmaplist, cmap.N
-        )
-        bounds = np.linspace(0, len(self.res.history), len(self.res.history)+1)   # define the bins
-        norm = mpl.colors.BoundaryNorm(bounds, cmap.N)        # normalize
-        
-        # define scatter plot axes
-        ax.scatter(TAF, loss_millions, c=colors, cmap=cmap, norm=norm, alpha=0.5)
-        ax.set_title("Particle Costs evaluated in Optimization History\nOptimal Point shown in Red")
-        ax.set_xlabel("Sum of Long-term Water Management Option Fixed Yield Augmentation (acre-feet/year")
-        ax.set_xscale('log')
-        ax.set_ylabel("Expected Costs and Losses ($ Million)")
-        ax.ticklabel_format(axis="y", style="sci", useOffset=False)
-        
-        # create a second axes for the colorbar
-        ax2 = fig.add_axes([0.95, 0.1, 0.03, 0.8])
-        cb = mpl.colorbar.ColorbarBase(
-            ax2, cmap=cmap, norm=norm,
-            spacing='proportional', ticks=bounds, boundaries=bounds, format='%1i'
-        )
-        ax2.set_ylabel('Iteration Number', size=12)
-        
-        # plot the best result in red 
-        ax.scatter(x=sum(self.res.X), y=self.res.F*10**-6, c='red', marker="o")
-#        ax.text(s=f'Fmin = {int(self.res.F)}',
-#                x=sum(self.res.X),y=(self.res.F*10**-6)-2,
-#                c='red',size=8)
-        ax.hlines(y=self.F_zero*10**-6, xmin=min(TAF), xmax=max(TAF),
-                  color='red', linestyle='--', label='F([O]*8)')
-#        ax.text(s=f'F0 = {int(self.F_zero)}',
-#                x=min(TAF)*1.05,y=(self.F_zero*10**-6)+1,
-#                c='red',size=8)
-        if test: 
-            plt.savefig('tests/test_graphic.png', bbox_inches='tight')
-        else:
-            if save:
-                population = self.res.algorithm.pop_size
-                n_iter = self.res.algorithm.n_iter
-                start_time = round(self.res.algorithm.start_time)
-                contractor = self.modelLogic.contractor.replace(" ", "")
-                year = self.modelLogic.inputData.futureYear
-                figname = f'graphics/{contractor}-{year}_optimization_p-{population}_n-{n_iter}_{start_time}.png'
-                plt.tight_layout()
-                plt.savefig(figname, bbox_inches='tight') # TODO: configure png so it doesn't cut off data for some plots
-                return figname
-            else: 
-                plt.show()
+            # assign plot variables
+            TAF = self.plotData['x']
+            loss_millions = self.plotData['y_millions']
+            colors = self.plotData['colors']
+
+            # matplotlib
+            fig, ax = plt.subplots(1, 1, figsize=(6, 6))          # setup the plot
+            cmap = plt.cm.viridis                                 # define the colormap
+            cmaplist = [ cmap(i) for i in range(cmap.N) ]         # extract all colors from map
+            cb = cmap = mpl.colors.LinearSegmentedColormap.from_list(  # create the new map
+                'Custom cmap', cmaplist, cmap.N
+            )
+            bounds = np.linspace(0, len(self.res.history), len(self.res.history)+1)   # define the bins
+            norm = mpl.colors.BoundaryNorm(bounds, cmap.N)        # normalize
+            
+            # define scatter plot axes
+            ax.scatter(TAF, loss_millions, c=colors, cmap=cmap, norm=norm, alpha=0.5)
+            ax.set_title("Particle Costs evaluated in Optimization History\nOptimal Point shown in Red")
+            ax.set_xlabel("Sum of Long-term Water Management Option Fixed Yield Augmentation (acre-feet/year")
+            ax.set_xscale('log')
+            ax.set_ylabel("Expected Costs and Losses ($ Million)")
+            ax.ticklabel_format(axis="y", style="sci", useOffset=False)
+            
+            # create a second axes for the colorbar
+            ax2 = fig.add_axes([0.95, 0.1, 0.03, 0.8])
+            mpl.colorbar.ColorbarBase(
+                ax2, cmap=cmap, norm=norm,
+                spacing='proportional', ticks=bounds, boundaries=bounds, format='%1i'
+            )
+            ax2.set_ylabel('Iteration Number', size=12)
+            
+            # plot the best result in red 
+            ax.scatter(x=sum(self.res.X), y=self.res.F*10**-6, c='red', marker="o")
+            # ax.text(s=f'Fmin = {int(self.res.F)}',
+            #         x=sum(self.res.X),y=(self.res.F*10**-6)-2,
+            #         c='red',size=8)
+            ax.hlines(y=self.F_zero*10**-6, xmin=min(TAF), xmax=max(TAF),
+                    color='red', linestyle='--', label='F([O]*8)')
+            # ax.text(s=f'F0 = {int(self.F_zero)}',
+            #         x=min(TAF)*1.05,y=(self.F_zero*10**-6)+1,
+            #         c='red',size=8)
+            if test: 
+                plt.savefig('tests/test_graphic.png', bbox_inches='tight')
+            else:
+                if save:
+                    population = self.res.algorithm.pop_size
+                    n_iter = self.res.algorithm.n_iter
+                    start_time = round(self.res.algorithm.start_time)
+                    contractor = self.modelLogic.contractor.replace(" ", "")
+                    year = self.modelLogic.inputData.futureYear
+                    figname = f'graphics/{contractor}-{year}_optimization_p-{population}_n-{n_iter}_{start_time}.png'
+                    plt.tight_layout()
+                    plt.savefig(figname, bbox_inches='tight') # TODO: configure png so it doesn't cut off data for some plots
+                    return figname
+                else: 
+                    plt.show()
+        except Exception as e: 
+            print("Couldn't plot. Aborting...")
+            print(e)
 
 
