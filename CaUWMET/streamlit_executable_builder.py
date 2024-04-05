ddef2ad203673cc2d32f6995ae0696b888f7a9aa
Daly <Kensey.Daly@jacobs.com>
2024-04-04 20:06:21 -0700

    Development of files to build executables for the model logic and Streamlit data visualizer.

--- CaUWMET/streamlit_executable_builder.py
+++ CaUWMET/streamlit_executable_builder.py
@@ -4,7 +4,6 @@ import streamlit
 import streamlit.web.cli as stcli
 import os, sys
 
-
 def resolve_path(path):
     resolved_path = os.path.abspath(os.path.join(os.getcwd(), path))
     return resolved_path
