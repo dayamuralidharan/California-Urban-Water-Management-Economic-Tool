f79628aa773d33b74e7dcf27df85dba7421119bc
kensey <kensey.daly@jacobs.com>
2024-04-27 12:31:20 -0700

    get exe working on vm

--- CaUWMET/streamlit_executable_builder.py
+++ CaUWMET/streamlit_executable_builder.py
@@ -4,22 +4,27 @@ import streamlit
 import streamlit.web.cli as stcli
 import os, sys
 
-def resolve_path(path):
-    resolved_path = os.path.abspath(os.path.join(os.getcwd(), path))
-    return resolved_path
+def getPort() -> str:
+    if 'CAUWMET_PORT' in os.environ:
+        return os.environ['CAUWMET_PORT']
+    return "8080"
 
+def resolve_path(path: str) -> str:
+    return os.path.abspath(os.path.join(os.getcwd(), path))
 
 if __name__ == "__main__":
     try:
+        port = getPort()
         logging.basicConfig(filename = 'logfile.log', filemode = 'w', level = logging.INFO)
-
-        logging.info("Running streamlit app....")
+        logging.info("Running streamlit app on port %s", port)
         sys.argv = [
             "streamlit",
             "run",
             resolve_path("app.py"),
             "--global.developmentMode=false",
+            "--server.port={}".format(port)
         ]
         sys.exit(stcli.main())
     except Exception as exception:
         logging.error(traceback.format_exc())
+
